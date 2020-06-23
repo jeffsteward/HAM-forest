cebd224f3d8fed9cc02bb36baf7affd8905d1b12
Jeff Steward <steward.jeff@gmail.com>
2020-06-22 20:20:38 -0400

    enable annotation resizing

--- server/public/resources/interactives/apparatus.js
+++ server/public/resources/interactives/apparatus.js
@@ -17,12 +17,12 @@ class Apparatus {
             {name: 'branch', color: 'red', x: 0, y: 0, width: 25, height: 150, fragment: '', imageUrl: "", id: `${this.id}_branch`}
         ];        
 
+        this._sizerBoxWidth = 10;
+        this._maxScaleWidth = 100;
+
         this._currentManifestIndex = 0;
         this._collection;
         
-        this._dragX0 = 0;
-        this._dragY0 = 0;
-
         this.parentElement = containerElement;
         this.container;
         this.buttonNext;
@@ -77,29 +77,46 @@ class Apparatus {
         this.container.append(toolbar);
         this.container.append(this.image);
 
-        let drag = d3.drag();        
+        let dragMove = d3.drag()
+                    .on('end', this._dragEnded.bind(this))
+                    .on('drag', this._dragged.bind(this));
+
+        let dragResize = d3.drag()
+                    .on('drag', this._scaleAnnotation.bind(this));
 
         this.annotationLayer = d3.select(this.container)
                                     .append('svg')
                                     .attr('id', `${this.id}_annotationLayer`)
                                     .style('position', 'absolute');
                             
-        this.annotationLayer.selectAll('rect')
-                    .data(this.annotations)
-                .enter().append('rect')
-                    .attr('stroke', (d) => {return d.color})
-                    .attr('stroke-width', 5)
-                    .attr('fill', 'transparent')
-                    .attr('x', (d) => {return d.x})
-                    .attr('y', (d) => {return d.y})
-                    .attr('width', (d) => {return d.width})
-                    .attr('height', (d) => {return d.height})
-                    .attr('id', (d) => {return d.id})
-                    .call(drag
-                            .on('start', this._dragStarted.bind(this))
-                            .on('end', this._dragEnded.bind(this))
-                            .on('drag', this._dragged.bind(this)))
-                .append('rect');
+        let annotationGroups = this.annotationLayer.selectAll('g')
+                                            .data(this.annotations)
+                                        .enter().append('g');
+
+        // the box containing the annotation
+        annotationGroups.append('rect')
+            .attr('stroke', (d) => {return d.color})
+            .attr('stroke-width', 5)
+            .attr('fill', 'transparent')
+            .attr('cursor', 'move')
+            .attr('x', (d) => {return d.x})
+            .attr('y', (d) => {return d.y})
+            .attr('width', (d) => {return d.width})
+            .attr('height', (d) => {return d.height})
+            .attr('id', (d) => {return d.id})
+            .call(dragMove);
+            
+        // the drag hangle for resizing the annotation
+        annotationGroups.append('rect')
+            .attr('fill', (d) => {return d.color})
+            .attr('fill-opacity', 0.5)
+            .attr('cursor', 'se-resizE')
+            .attr('x', (d) => {return d.width - (this._sizerBoxWidth/2)})
+            .attr('y', (d) => {return d.height - (this._sizerBoxWidth/2)})
+            .attr('width', this._sizerBoxWidth)
+            .attr('height', this._sizerBoxWidth)
+            .attr('id', (d) => {return `${d.id}_sizer`})
+            .call(dragResize);
                             
         this.parentElement.append(this.container);
     }
@@ -193,32 +210,23 @@ class Apparatus {
             y = 0; 
         }
     
+        d3.select(`${annotation}_sizer`)
+            .transition()
+                .duration(250)
+                .attr('x', x + bbox.width - (this._sizerBoxWidth/2))
+                .attr('y', y + bbox.height - (this._sizerBoxWidth/2));
+
         a.transition()
             .duration(250)
             .attr('x', x)
             .attr('y', y)
             .on('end', () => {this._updateAnnotation(annotation)});
     }
-
-    _scaleAnnotation(annotation) {        
-        // let zoomInFactor = 1.15;
-        // let zoomOutFactor = 0.85;
-        // let a = d3.select(annotation);
-        // let w = a.attr('width');
-        // let h = a.attr('height');
-
-        // if ((w * factor < 100) && (w * factor > 10)) {
-        //     a.attr('width', w * factor)
-        //         .attr('height', h * factor);
-            
-        //     moveAnnotationInbounds(annotation);
-        // }
-    }
-
+    
     _updateAnnotation(annotation) {
         let bbox = d3.select(annotation).node().getBBox();
         let index = (annotation.indexOf('leaf') > 0 ? 0 : 1); 
-    
+        
         this.annotations[index].x = bbox.x;
         this.annotations[index].y = bbox.y;
         this.annotations[index].width = bbox.width;
@@ -226,20 +234,48 @@ class Apparatus {
         this.annotations[index].fragment = Math.round(bbox.x * this.scaleFactor) + ',' + Math.round(bbox.y * this.scaleFactor) + ',' + Math.round(bbox.width * this.scaleFactor) + ',' + Math.round(bbox.height * this.scaleFactor);    
         this.annotations[index].imageUrl = `${this.imageUri}/${this.annotations[index].fragment}/full/0/default.jpg`;
     }
+    
+    _scaleAnnotation(d) {      
+        let annotation = d3.select(`#${d.id}`);
+        let annotationSizer = d3.select(`#${d.id}_sizer`);    
+        let scaleFactor = (d.height / d.width);
 
-    _dragged(e) {
-        d3.select(`#${e.id}`)
-            .attr('x', (d3.event.x - this._dragX0))
-            .attr('y', (d3.event.y - this._dragY0));
-    }    
+        let dragY = 0;
+        let dragX = 0;
+        let width;
+        let height;
 
-    _dragStarted(e) {
-        this._dragX0 = d3.event.x - d3.select(`#${e.id}`).node().getBBox().x;
-        this._dragY0 = d3.event.y - d3.select(`#${e.id}`).node().getBBox().y;
+        // is it more vertical than horizontal
+        if (scaleFactor > 1.0) {
+            dragX = Math.max(d.x + 15, Math.min(d.x + (this._maxScaleWidth/2), (d.x + d.width + d3.event.dx)));
+        } else {
+            dragX = Math.max(d.x + 15, Math.min(d.x + this._maxScaleWidth, (d.x + d.width + d3.event.dx)));
+        }
+        width = dragX - d.x;
+        height = width * scaleFactor;
+        dragY = d.y + height;
+
+        annotationSizer.attr('x', dragX - (this._sizerBoxWidth/2))
+                        .attr('y', dragY - (this._sizerBoxWidth/2));
+
+        annotation.attr('width', width)
+                    .attr('height', height);
+
+        this._updateAnnotation(`#${d.id}`);
     }
     
-    _dragEnded(e) {
-        this._moveAnnotationInbounds(`#${e.id}`);
+    _dragged(d) {
+        d3.select(`#${d.id}`)
+            .attr('x', d3.event.x)
+            .attr('y', d3.event.y);
+
+        d3.select(`#${d.id}_sizer`)
+            .attr('x', d3.event.x + d.width - (this._sizerBoxWidth/2))
+            .attr('y', d3.event.y + d.height - (this._sizerBoxWidth/2));
+    }    
+
+    _dragEnded(d) {
+        this._moveAnnotationInbounds(`#${d.id}`);
     }
 
     _cast() {
